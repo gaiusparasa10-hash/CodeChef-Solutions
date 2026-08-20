de3d9c646b1bb51e4ba13c8b9090dd25fb6541f6
gaiusparasa10-hash <gaiusparasa10@gmail.com>
2026-08-20 11:19:59 +0530

    Add solution for LeetCode - Merge Intervals

--- LeetCode/Medium/0056 Merge Intervals/Solution.java
+++ LeetCode/Medium/0056 Merge Intervals/Solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        // Sort by start time
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         List<int[]> result = new ArrayList<>();

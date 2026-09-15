4c6648183a801bb68958557747ac5c524f1f5b61
gaiusparasa10-hash <gaiusparasa10@gmail.com>
2026-09-15 11:57:10 +0530

    Add solution for LeetCode - Two Sum

--- LeetCode/Easy/0001 Two Sum/Solution.java
+++ LeetCode/Easy/0001 Two Sum/Solution.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.)
+        for(int i = 0; i < nums.length(); i++){
+            
+        }
     }
 }
